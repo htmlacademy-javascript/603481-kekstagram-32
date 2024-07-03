997e268591bd5a418c02aae5d7eec0ab4216c95f
ekburalec <drmax782@gmail.com>
2024-07-03 19:00:58 +0500

    Генерация данных. (#3)
    * Создает файл main.js
    
    * Генерация данных
    
    * фиксы
    
    * Merge remote-tracking branch 'origin/master' into module4-task1
    
    * пофиксил баг

--- js/functions.js
+++ js/functions.js
@@ -1,22 +1,22 @@
-const checkStringLength = function (string, stringLength) {
-  return string.length <= stringLength
-}
+// const checkStringLength = function (newstring, stringLength) {
+//   return newstring.length <= stringLength
+// }
 
-console.log(checkStringLength('Hello, World!', 14));
-console.log(checkStringLength('Hello', 4));
+// console.log(checkStringLength('Hello, World!', 14));
+// console.log(checkStringLength('Hello', 4));
 
-let isPalindrome = function(string) {
-  string = string.toLowerCase().replaceAll(' ', '');
-  let palindrome = ''
+// let isPalindrome = function(newstring) {
+//   newstring = newstring.toLowerCase().replaceAll(' ', '');
+//   let palindrome = ''
 
-  for (let i = string.length - 1; i >= 0; i--) {
-    palindrome += string[i]
-  }
+//   for (let i = newstring.length - 1; i >= 0; i--) {
+//     palindrome += newstring[i]
+//   }
 
- return palindrome === string;
-}
+//  return palindrome === newstring;
+// }
 
-console.log(isPalindrome("коту тащат уток"));
-console.log(isPalindrome("кекстограмм"));
-console.log(isPalindrome("довод"));
-console.log(isPalindrome("кекс"));
+// console.log(isPalindrome("коту тащат уток"));
+// console.log(isPalindrome("кекстограмм"));
+// console.log(isPalindrome("довод"));
+// console.log(isPalindrome("кекс"));
