f55efb4438b10b7f16b1e1e9872a8148a5532998
ekburalec <drmax782@gmail.com>
2024-07-07 21:52:42 +0500

    Добавляет генерацию временных данных. (#1)

--- js/functions.js
+++ js/functions.js
@@ -1,22 +1,22 @@
-const checkStringLength = function (string, stringLength) {
-  return string.length <= stringLength
-}
+// const checkStringLength = function (string, stringLength) {
+//   return string.length <= stringLength
+// }
 
-console.log(checkStringLength('Hello, World!', 14));
-console.log(checkStringLength('Hello', 4));
+// console.log(checkStringLength('Hello, World!', 14));
+// console.log(checkStringLength('Hello', 4));
 
-let isPalindrome = function(string) {
-  string = string.toLowerCase().replaceAll(' ', '');
-  let palindrome = ''
+// let isPalindrome = function(string) {
+//   string = string.toLowerCase().replaceAll(' ', '');
+//   let palindrome = ''
 
-  for (let i = string.length - 1; i >= 0; i--) {
-    palindrome += string[i]
-  }
+//   for (let i = string.length - 1; i >= 0; i--) {
+//     palindrome += string[i]
+//   }
 
- return palindrome === string;
-}
+//  return palindrome === string;
+// }
 
-console.log(isPalindrome("коту тащат уток"));
-console.log(isPalindrome("кекстограмм"));
-console.log(isPalindrome("довод"));
-console.log(isPalindrome("кекс"));
+// console.log(isPalindrome("коту тащат уток"));
+// console.log(isPalindrome("кекстограмм"));
+// console.log(isPalindrome("довод"));
+// console.log(isPalindrome("кекс"));
